0de7bbafe8dc3cc442cbc2438fdc527cc277628f
Julien <julien.sebag@me.com>
2022-03-19 18:50:53 +0100

    Added proxy for API

--- client/src/api/http-common.js
+++ client/src/api/http-common.js
@@ -1,10 +1,8 @@
 // Packages
 import axios from "axios"
 
-const apiUrl = "http://localhost:5005"
-
 export default axios.create({
-    baseURL: `${apiUrl}/api`,
+    baseURL: "/api",
     headers: {
         "Content-type": "application/json",
     },
